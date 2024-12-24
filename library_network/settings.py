c4d0ccb5de5a9ab1f879df5b7f7e500b446d6687
katiko <k7a8t0e9@gmail.com>
2024-12-25 01:30:00 +0300

    2

--- library_network/settings.py
+++ library_network/settings.py
@@ -137,3 +137,4 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000']
